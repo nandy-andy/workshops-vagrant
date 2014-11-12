642643e173860195040944a1819d299807821b94
nAndy <nandy@wikia-inc.com>
2014-11-12 13:32:49 +0100

    added memcache and its checks in index.php

--- public/index.php
+++ public/index.php
@@ -11,27 +11,36 @@
          echo 'Welcome to Wikia workshop virtual machine!' . "\n";
          
          echo "\n";
-         echo "Apache status:\t\t" . STATUS_OK . "\n";
-         echo "PHP status:\t\t" . STATUS_OK . "\n";
+         echo "Apache status:\t\t\t" . STATUS_OK . "\n";
+         echo "PHP status:\t\t\t" . STATUS_OK . "\n";
 
          try {
             $pdo = new PDO('mysql:dbname=wikia_workshops;host=localhost', 'root', 'rootpass');
             $pdo->exec("SET CHARACTER SET utf8");
-            echo "DB status:\t\t" . STATUS_OK . "\n";
+            echo "DB status:\t\t\t" . STATUS_OK . "\n";
          } catch(PDOException $e) {
-            echo "DB status:\t\t" . STATUS_FAILED . "\n";
+            echo "DB status:\t\t\t" . STATUS_FAILED . "\n";
             echo 'Database connection failed: ' . $e->getMessage() . "\n";
          } catch(Exception $e) {
-            echo "DB status:\t\t" . STATUS_FAILED . "\n";
+            echo "DB status:\t\t\t" . STATUS_FAILED . "\n";
             echo 'Something went wrong: ' . $e->getMessage() . "\n";
          }
 
+         $memc = new Memcache();
+         $memc->pconnect('localhost', 11211);
+         $memcStatus = $memc->getStats() !== false ? STATUS_OK : STATUS_FAILED;
+         echo "Memcached status:\t\t$memcStatus\n";
+
+         $memc = new Memcache();
+         $memcPhpStatus = is_object($memc) ? STATUS_OK : STATUS_FAILED;
+         echo "Memcached PHP client status:\t$memcPhpStatus\n";
+
          $xdebugEnabled = xdebug_is_enabled();
          $xdebugStatus = $xdebugEnabled ? STATUS_OK : STATUS_FAILED;
-         echo "XDebug status:\t\t$xdebugStatus\n";
+         echo "XDebug status:\t\t\t$xdebugStatus\n";
 
          $webgrindStatus = file_exists( '/var/www/webgrind/index.php' ) ? STATUS_OK : STATUS_FAILED;
-         echo "Webgrind status:\t$webgrindStatus\n";
+         echo "Webgrind status:\t\t$webgrindStatus\n";
       ?>
       </pre>
    </body>
